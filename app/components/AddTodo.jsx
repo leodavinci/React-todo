f4593a32627ceb3c3e8a2f670cf1f7ac9fbdd79d
Mayank Joshi <mayank@aisle.co>
2018-04-26 00:12:47 +0530

    changes

---
+++ app/components/AddTodo.jsx
@@ -0,0 +1,24 @@
+var React = require('react');
+
+var AddTodo = React.createClass({
+    onAddNewTodo: function(e) {
+        e.preventDefault();
+        var todo = this.refs.todo.value;
+        if(todo.length > 0) {
+            this.refs.todo.value = '';
+            this.props.onAddTodo(todo);
+        } else {
+            this.refs.todo.focus();
+        }
+    },
+    render: function() {
+        return (
+            <form onSubmit={this.onAddNewTodo}>
+                <input type="text" ref="todo" placeholder="add something..."/>
+                <button className="button expanded">Add New Todo</button>
+            </form>
+        );
+    }
+});
+
+module.exports = AddTodo;
