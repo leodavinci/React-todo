eb8745b8eac72a3fb7d3f67b01939da483e82469
Mayank Joshi <mayank@aisle.co>
2018-04-27 00:59:39 +0530

    changes

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -20,5 +20,30 @@ module.exports = {
         } else {
             return [];
         }
+    },
+    filterTodos: function(todos, showCompleted, searchText){
+        var filteredTodos = todos;
+
+        // filter by showCompleted
+        filteredTodos = filteredTodos.filter((todo) => {
+            return !todo.completed || showCompleted;
+        });
+        // filter by searchText
+        filteredTodos = filteredTodos.filter((todo) => {
+            var position = todo.text.toLowerCase().indexOf(searchText);
+            return searchText.length === 0 || position >= 0;
+        });
+
+        // sort with non completed first
+        filteredTodos.sort((a, b) => {
+            if(!a.completed && b.completed) {
+                return -1;
+            } else if(a.completed && !b.completed) {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
+        return filteredTodos;
     }
 };
