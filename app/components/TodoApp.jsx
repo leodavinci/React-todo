87216f5028ba398507540c03bb304570d0951e4b
Mayank Joshi <mayank@aisle.co>
2018-05-05 00:35:07 +0530

    final

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,55 +1,20 @@
 var React = require('react');
-var uuid = require('node-uuid');
-var moment = require('moment');
 
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
 var TodoSearch = require('TodoSearch');
-var TodoAPI = require('TodoAPI');
 
 var TodoApp = React.createClass({
-    getInitialState: function() {
-        return {
-            todos: TodoAPI.getTodos(),
-            showCompleted: false,
-            searchText: ''
-        };
-    },
-    componentDidUpdate: function() {
-        TodoAPI.setTodos(this.state.todos);
-    },
-    handleSearch: function(showCompleted, searchText) {
-        this.setState({
-            showCompleted: showCompleted,
-            searchText: searchText.toLowerCase()
-        });
-    },
-    handleAddTodo: function(text) {
-        this.setState({
-            todos: [
-                ...this.state.todos,
-                {
-                    id: uuid(),
-                    text: text,
-                    completed: false,
-                    createdAt: moment().unix(),
-                    completedAt: undefined
-                }
-            ]
-        });
-    },
     render: function() {
-        var {todos, showCompleted, searchText} = this.state;
-        var filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
         return (
             <div>
                 <h1 className="page-title">Todo App</h1>
                 <div className="row">
                     <div className="column small-centered small-11 medium-6 large-5">
                         <div className="container">
-                            <TodoSearch onSearch={this.handleSearch}/>
+                            <TodoSearch/>
                             <TodoList/>
-                            <AddTodo onAddTodo={this.handleAddTodo}/>
+                            <AddTodo/>
                         </div>
                     </div>
                 </div>
